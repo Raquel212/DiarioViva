0dec8ac33cc4d7f94a98f6338727085ea87527f7
Raquel Martins <raquelcristinamartins750@gmail.com>
2025-07-16 19:13:21 -0300

    feat: adiciona rota para página de cadastro no routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,7 @@ import HomePrincipal from './pages/HomePrincipal/Home';
 import Login from './pages/Login/Login';
 import Header from './components/Header/Header'; 
 import Footer from './components/Footer/Footer'; 
+import Cadastro from './pages/Cadastro/Cadastro.jsx';
 
 function AppRoutes() {
   return (
@@ -11,6 +12,7 @@ function AppRoutes() {
       <Routes>
         <Route path="/" element={<HomePrincipal />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
       </Routes>
       <Footer/>
     </BrowserRouter>
