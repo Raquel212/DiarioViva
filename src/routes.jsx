1c7735095d3612713e5b4963879b1e5c3939cdbf
Raquel Martins <raquelcristinamartins750@gmail.com>
2025-07-23 20:07:24 -0300

    feat: adiciona rota para tela de EditarPaciente

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePrincipal from './pages/HomePrincipal/Home';
 import Login from './pages/Login/Login';
 import Cadastro from './pages/Cadastro/Cadastro';
+import EditarPerfilPaciente from './pages/EditarPaciente/EditarPaciente';
 
 function AppRoutes() {
   return (
@@ -10,6 +11,7 @@ function AppRoutes() {
         <Route path="/" element={<HomePrincipal />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/editarPerfilPaciente" element={<EditarPerfilPaciente />} />
       </Routes>
     </BrowserRouter>
   );
