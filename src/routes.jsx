888f9bc21a5a4cd1d3e204160857e087891a9265
Raquel Martins <raquelcristinamartins750@gmail.com>
2025-07-23 21:45:47 -0300

    feat: adiciona rota para tela de editar perfil profissional

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,7 @@ import HomePrincipal from './pages/HomePrincipal/Home';
 import Login from './pages/Login/Login';
 import Cadastro from './pages/Cadastro/Cadastro';
 import EditarPerfilPaciente from './pages/EditarPaciente/EditarPaciente';
+import EditarPerfilProfissional from './pages/EditarProfissional/EditarProfissional';
 
 function AppRoutes() {
   return (
@@ -12,6 +13,7 @@ function AppRoutes() {
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/editarPerfilPaciente" element={<EditarPerfilPaciente />} />
+        <Route path="/editarPerfilProfissional" element={<EditarPerfilProfissional />} />
       </Routes>
     </BrowserRouter>
   );
