1aaa904569e348f10f14e13496a7ae3a8f708894
Raquel Martins <raquelcristinamartins750@gmail.com>
2025-09-22 09:51:00 -0300

    fix: forçar fallback de rotas SPA no Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  base: '/',
   build: {
     outDir: 'dist',
   },
+  server: {
+    historyApiFallback: true, 
+  },
+  preview: {
+    historyApiFallback: true, 
+  }
 })
