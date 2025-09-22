966d61225c4be9e266e733f1a1147b75d6e8a11a
Raquel Martins <raquelcristinamartins750@gmail.com>
2025-09-22 00:39:30 -0300

    feat: Ajuste do caminho no CSS

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
-import "./Login.css";
+import "./login.css";
 
 function Login() {
   const navigate = useNavigate();
